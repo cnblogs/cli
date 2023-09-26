c80b2a2c3132a3a2ad061a751c55f704864f2e84
Thaumy <thaumy@outlook.com>
2023-09-26 13:00:21 +0800

    refactor: rm unnecessary constraint

--- src/infra/vec.rs
+++ src/infra/vec.rs
@@ -2,10 +2,7 @@ pub trait VecExt<T> {
     fn chain_push(self, item: T) -> Vec<T>;
 }
 
-impl<T> VecExt<T> for Vec<T>
-where
-    T: Clone,
-{
+impl<T> VecExt<T> for Vec<T> {
     #[inline]
     fn chain_push(mut self, item: T) -> Self {
         self.push(item);
