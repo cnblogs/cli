ec62483b3ccc345edc07d90bdc427f5fd1911571
RocSun <710989028@qq.com>
2024-01-01 23:56:32 +0800

    refactor: add lib and bin files.

---
+++ src/apis/mod.rs
@@ -0,0 +1,14 @@
+//! cnblogs 闪存接口模块
+//!
+//! 封装[cnblogs Api](https://api.cnblogs.com/Help#0aee001a01835c83a3277a500ffc9040)至以下模块中：
+//!
+//! - statuses: 闪存相关api。
+//! - blogs: 博客相关
+//! - news: 新闻相关
+//! - questions: 问题相关
+//! - edu: edu 相关
+//! - user: 用户相关
+//! - token: 认证相关
+//! - marks: 收藏相关
+
+pub mod statuses;
